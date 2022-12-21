d6ac8387559b5462314fbf6c9a121412e02fd011
Eumanuel <44380634+Eumanuel@users.noreply.github.com>
2022-12-21 10:27:49 -0300

    Added simple home
    Todo better homepage

--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -1,20 +1,8 @@
 import './home.css'
 
-import { Airplane, Code } from 'phosphor-react'
-
 
 export default function Home() {
   return (
-    <div className="home">
-      <div className="hoami">
-        <h1>Emanuel Ribeiro da Silva</h1>
-        <img className='hoamiPhoto' src="./fotoPerfil.jpg" alt="Emanuel Ribeiro" />
-        <p>Full Stack Jr. C# & JS</p>
-        <p className='hoamiPhrase'><Airplane size={32} weight="thin"/>&nbsp;&nbsp;Amante de aviação e programação&nbsp;&nbsp;<Code size={32} weight="thin"/></p>
-        <p>Eu comecei a programar bem cedo na vida, criando meus scripts Batch para automatizar coisas simples no computador, de lá parti para o C++ e Python, mas o meu foco verdadeiro ficou em NodeJs e C#.</p>
-        <p>Tenho experiência administrando serviços nos seguintes Clouds: Umbler, Google, Azure, AWS, Oracle, OVH, Kimsufi, Digital Ocean, SoYouStart, ClouFlare (Pages e Workers) e WiseCP, além de uma sólida experiência administrando WordPress, E-mails, domínios e em suporte ao cliente.</p>
-        <p></p>
-      </div>
-    </div>
+    <div>Em construção... (Dá um bisu no GitHub!)</div>
   )
 }
