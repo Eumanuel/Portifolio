ca5a34e4624807a3d3e731cc002ad9948f95f6a8
Eumanuel <44380634+Eumanuel@users.noreply.github.com>
2022-12-21 06:31:12 -0300

    Selfhost the gif

--- src/app/pages/NotFound/NotFound.jsx
+++ src/app/pages/NotFound/NotFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className='errorPage'>
         <h1 className='topText'>Opaa, onde cê pensa que tá indo?</h1>
-        <img src="https://onedrive.live.com/download?cid=D3A14AD9A72B3132&resid=D3A14AD9A72B3132%211598&authkey=AGy93fi6XvVSXZs" alt="" />
+        <img src="./404.gif" alt="" />
         <div>404</div>
     </div>
   )
