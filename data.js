7d714220d67047828fd597c0e6ed33fbeef0108a
Eumanuel <44380634+Eumanuel@users.noreply.github.com>
2022-12-26 20:15:24 -0300

    Dados para criar objetos dinâmicos

---
+++ data.js
@@ -0,0 +1,59 @@
+export const languages = [
+    {
+        id: 0,
+        name: "C#",
+        logo: "csharp.png",
+        description: "1 ano de experiência profissional em C# na Umbler"
+    },
+    {
+        id: 1,
+        name: "NodeJs",
+        logo: "nodejs.png",
+        description: "3 anos de experiência em NodeJs fazendo APIs e ferramentas."
+    },
+    {
+        id: 2,
+        name: "React",
+        logo: "react.png",
+        description: "1 ano de experiência em React criando paineis de controle e LandingPages. (Esta página foi escrita em react e está disponível em meu GitHub!)"
+    },
+    {
+        id: 3,
+        name: "Blazor",
+        logo: "blazor.png",
+        description: "1 ano de experiência profissional em Blazor na Umbler."
+    },
+    {
+        id: 4,
+        name: "Discord.js",
+        logo: "djs.png",
+        description: "2 anos de experiência desenvolvendo bots para discord"
+    }
+]
+
+export const tools = [
+    {
+        id: 0,
+        name: "Prometheus",
+        logo: "prometheus.png",
+        description: "1 ano de experiência exportando dados para uso pessoal e profissional."
+    },
+    {
+        id: 1,
+        name: "Grafana",
+        logo: "grafana.png",
+        description: "1 ano de experiência criando gráficos para msotrar meus dados exportados com o Prometheus."
+    },
+    {
+        id: 2,
+        name: "After Effects",
+        logo: "aftereffects.png",
+        description: "Alguns anos de experiência em projetos pessoais, além de experiência profissional com a Umbler."
+    },
+    {
+        id: 2,
+        name: "Premiere Pro",
+        logo: "premiere.jpg",
+        description: "Alguns anos de experiência em projetos pessoais, além de experiência profissional com a Umbler."
+    },
+]
